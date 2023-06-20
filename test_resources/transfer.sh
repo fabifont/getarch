ef3c2c325b4234a7ce736aec8e30bb0648c801bb
fabifont <fabio.fontana@mailbox.org>
2023-06-21 00:54:10 +0200

    feat: add support for ext4; fix hooks order; better ci workflow; fix #1

--- test_resources/transfer.sh
+++ test_resources/transfer.sh
@@ -6,5 +6,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 # Get the parent directory of the current script
 PARENT_DIR="$(dirname "$SCRIPT_DIR")"
 
+ssh-keygen -f "/home/${USER}/.ssh/known_hosts" -R "[localhost]:2222" || true
 sshpass -p "a" scp -P 2222 -o StrictHostKeyChecking=no -r ./dist/getarch root@localhost:~
 sshpass -p "a" scp -P 2222 -o StrictHostKeyChecking=no -r ./examples/config.json root@localhost:~
